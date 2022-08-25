fdb8fed51807f120a85e5f51a783095335b2b98d
Omikorin <michal.k2001@gmail.com>
2022-08-26 01:32:45 +0200

    add backend

---
+++ backend/config/middlewares.ts
@@ -0,0 +1,11 @@
+export default [
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
