b5e22ee7eaedef6ee67a83ad1d9b286e180d04a5
dawidos234 <dawidos234@gmail.com>
2022-11-27 23:03:57 +0100

    remove dark mode

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -20,21 +20,21 @@ export default function Navbar() {
             </FlowbiteNavbar.Brand>
             <FlowbiteNavbar.Toggle />
             <FlowbiteNavbar.Collapse>
-                <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white bg-gray-dark:800 ">
                     <li>
-                        <Link href="/aktualnosci" className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Aktualności</Link>
+                        <Link href="/aktualnosci" className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0" aria-current="page">Aktualności</Link>
                     </li>
                     <li>
-                        <Link href="/posiedzenia" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Posiedzenia</Link>
+                        <Link href="/posiedzenia" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Posiedzenia</Link>
                     </li>
                     <li>
-                        <Link href="/uchwaly" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Uchwały</Link>
+                        <Link href="/uchwaly" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Uchwały</Link>
                     </li>
                     <li>
-                        <Link href="/struktura" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Struktura organizacyjna</Link>
+                        <Link href="/struktura" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Struktura organizacyjna</Link>
                     </li>
                     <li className='hidden'>
-                        <Link href="/sprawozdania" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Sprawozdania z prac</Link>
+                        <Link href="/sprawozdania" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Sprawozdania z prac</Link>
                     </li>
                 </ul>
             </FlowbiteNavbar.Collapse>
--- frontend/components/term-of-office-selector.tsx
+++ frontend/components/term-of-office-selector.tsx
@@ -23,8 +23,8 @@ const TermOfOfficeSelector: FC<TermOfOfficeSelectorProps> = (props) => {
     };
 
     return <div>
-        <label htmlFor="terms" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Wybierz kadencję</label>
-        <select onChange={handleChange} value={selectedTerm?.id} id="terms" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        <label htmlFor="terms" className="block mb-2 text-sm font-medium text-gray-900">Wybierz kadencję</label>
+        <select onChange={handleChange} value={selectedTerm?.id} id="terms" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block">
             {terms?.map((term) => (
                 <option value={term.id} key={term.id}>{term.attributes.term_of_office}</option>
             ))}
