33ab735f193d5892150d5f33f2fe90480c5ea908
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2022-12-03 16:35:43 +0100

    refactor(structure): use chakra components

--- frontend/src/pages/struktura.tsx
+++ frontend/src/pages/struktura.tsx
@@ -1,6 +1,14 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Loader from "../components/loader";
+import {
+  Center,
+  Heading,
+  ListItem,
+  Text,
+  UnorderedList,
+  VStack,
+} from "@chakra-ui/react";
 
 function OrganisationStructure({ Component, pageProps }: any) {
   const [composition, setComposition] = useState("");
@@ -11,26 +19,40 @@ function OrganisationStructure({ Component, pageProps }: any) {
   }, []);
 
   return (
-    <>
-      <div>
-        <strong>
-          Parlament jest najwyższym uchwałodawczym organem Samorządu
-          Studenckiego na Uczelni. W jego skład wchodzą:
-        </strong>
-        <br />
-        <br />
-        <ul className="list-disc">
-          <li>członkowie Studenckiego Kolegium Wyborczego,</li>
-          <li>członkowie Zarządu Parlamentu Studentów,</li>
-          <li>czterej członkowie Rady UOS, w tym Przewodniczący Rady UOS,</li>
-          <li>jeden delegat z Filii,</li>
-          <li>delegat z grupy Rad Mieszkańców z głosem doradczym,</li>
-          <li>członkowie Kolegium Senatorów Studenckich z głosem doradczym,</li>
-          <li>pełnomocnicy Zarządu Parlamentu Studentów z głosem doradczym,</li>
-          <li>dwóch delegatów z pozostałych Filii z głosem doradczym.</li>
-        </ul>
-      </div>
-    </>
+    <Center>
+      <VStack>
+        <Heading size="lg" mb={8}>
+          Struktura organizacyjna
+        </Heading>
+        <div>
+          <Text mb={8}>
+            Parlament jest najwyższym uchwałodawczym organem Samorządu
+            Studenckiego na Uczelni. W jego skład wchodzą:
+          </Text>
+
+          <UnorderedList>
+            <ListItem>członkowie Studenckiego Kolegium Wyborczego,</ListItem>
+            <ListItem>członkowie Zarządu Parlamentu Studentów,</ListItem>
+            <ListItem>
+              czterej członkowie Rady UOS, w tym Przewodniczący Rady UOS,
+            </ListItem>
+            <ListItem>jeden delegat z Filii,</ListItem>
+            <ListItem>
+              delegat z grupy Rad Mieszkańców z głosem doradczym,
+            </ListItem>
+            <ListItem>
+              członkowie Kolegium Senatorów Studenckich z głosem doradczym,
+            </ListItem>
+            <ListItem>
+              pełnomocnicy Zarządu Parlamentu Studentów z głosem doradczym,
+            </ListItem>
+            <ListItem>
+              dwóch delegatów z pozostałych Filii z głosem doradczym.
+            </ListItem>
+          </UnorderedList>
+        </div>
+      </VStack>
+    </Center>
   );
 }
 
