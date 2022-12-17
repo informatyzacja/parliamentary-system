11074eea56380eca97d4186605a4d053d313c7b4
Dawid Stachowiak <dawid@parlament.samorzad.pwr.edu.pl>
2022-12-17 04:00:01 +0000

    Upload files to Strapi outside Docker

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
+      host: env('DATABASE_HOST', 'postgres'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'parliamentarystrapi'),
       user: env('DATABASE_USERNAME', 'informatyzacja'),
