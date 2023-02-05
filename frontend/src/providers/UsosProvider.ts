a1f78b78ed189ce809b68f677a0e4efef3e40b64
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2023-02-05 17:59:57 +0100

    feat: move usos url to env variables

--- frontend/src/providers/UsosProvider.ts
+++ frontend/src/providers/UsosProvider.ts
@@ -1,3 +1,4 @@
+import { strict as assert } from "assert";
 import type { OAuthConfig, OAuthUserConfig } from "next-auth/providers";
 
 export interface UsosProfile extends Record<string, string> {
@@ -7,24 +8,26 @@ export interface UsosProfile extends Record<string, string> {
   student_number: string;
 }
 
+const USOS_BASE_URL = process.env.USOS_BASE_URL;
+
 export default function UsosProvider<P extends UsosProfile>(
   options: OAuthUserConfig<P>
 ): OAuthConfig<P> {
+  assert(USOS_BASE_URL, "USOS_BASE_URL is not defined");
+
   return {
     id: "usos",
     name: "USOS",
     type: "oauth",
     version: "1.0",
     authorization: {
-      url: "https://apps.usos.pwr.edu.pl/services/oauth/authorize",
+      url: `${USOS_BASE_URL}/services/oauth/authorize`,
     },
-    accessTokenUrl: "https://apps.usos.pwr.edu.pl/services/oauth/access_token",
-    requestTokenUrl:
-      "https://apps.usos.pwr.edu.pl/services/oauth/request_token?scopes=studies|email",
-    profileUrl:
-      "https://apps.usos.pwr.edu.pl/services/users/user?fields=first_name|last_name|sex|student_number|email",
+    accessTokenUrl: `${USOS_BASE_URL}/services/oauth/access_token`,
+    requestTokenUrl: `${USOS_BASE_URL}/services/oauth/request_token?scopes=studies|email`,
+    profileUrl: `${USOS_BASE_URL}/services/users/user?fields=first_name|last_name|sex|student_number|email`,
     userinfo: {
-      url: "https://apps.usos.pwr.edu.pl/services/users/user?fields=first_name|last_name|sex|student_number|email",
+      url: `${USOS_BASE_URL}/services/users/user?fields=first_name|last_name|sex|student_number|email`,
     },
     profile(profile) {
       return {
