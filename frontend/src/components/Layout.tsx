432627c23a5ec95ac2bc9d93d2ab19bf0e768e19
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2023-02-06 23:44:15 +0100

    fix: optimize fonts loading

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import { chakra, Container, VStack } from "@chakra-ui/react";
+import { Lato } from "@next/font/google";
 import type React from "react";
 
 import { Footer } from "./Footer";
@@ -8,9 +9,16 @@ interface LayoutProps {
   children: React.ReactNode;
 }
 
+const lato = Lato({
+  subsets: ["latin"],
+  weight: ["300", "400", "700"],
+  variable: "--font-lato",
+  display: "swap",
+});
+
 export default function Layout({ children }: LayoutProps) {
   return (
-    <VStack minH="100vh" spacing={8}>
+    <VStack minH="100vh" spacing={8} className={`${lato.variable}`}>
       <Container maxW="container.xl" flex={1} display="flex" flexDir="column">
         <Navbar />
         <chakra.main>{children}</chakra.main>
