45675d96cecafd7b1fe52540eafc7461a0b53544
Bartosz Gotowski <38581479+Rei-x@users.noreply.github.com>
2023-02-07 22:07:17 +0100

    feat(frontend): add profile dropdown (#63)
    * feat(frontend): add profile dropdown
    
    Changes default page to "aktualności" and adds profile dropdown
    
    * fix: fix navbar layout

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,8 +1,10 @@
-import { chakra, Container, VStack } from "@chakra-ui/react";
+import { Center, chakra, Container, Text, VStack } from "@chakra-ui/react";
 import { Lato } from "@next/font/google";
+import { useSession } from "next-auth/react";
 import type React from "react";
 
 import { Footer } from "./Footer";
+import { Loader } from "./Loader";
 import { Navbar } from "./Navbar";
 
 interface LayoutProps {
@@ -17,11 +19,23 @@ const lato = Lato({
 });
 
 export default function Layout({ children }: LayoutProps) {
+  const { status } = useSession();
+
   return (
     <VStack minH="100vh" spacing={8} className={`${lato.variable}`}>
+      <Navbar />
       <Container maxW="container.xl" flex={1} display="flex" flexDir="column">
-        <Navbar />
-        <chakra.main>{children}</chakra.main>
+        <chakra.main>
+          <>
+            {status === "authenticated" ? children : null}
+            {status === "loading" ? <Loader /> : null}
+            {status === "unauthenticated" ? (
+              <Center>
+                <Text>Aby korzystać z aplikacji musisz się zalogować</Text>
+              </Center>
+            ) : null}
+          </>
+        </chakra.main>
       </Container>
       <Footer />
     </VStack>
